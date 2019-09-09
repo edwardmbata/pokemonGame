7b8ae4eaa9ba8688f07180ba98369b37f84f17d1
em37555 <mbata.E@gmail.com>
2019-09-09 13:31:23 -0500

    sure
    sure

--- index.js
+++ index.js
@@ -11,13 +11,26 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+let controls;
 
 function preload() {
+  this.load.image("tiles", "../assets/tilesets/tuxon-sample-32px-extruded.png");
+  this.load.tilemapTiledJSON( "map", "../assets/tilemaps/tuxemon-town.json");
   // Runs once, loads up assets like images and audio
+
 }
 
 function create() {
   // Runs once, after all assets in preload are loaded
+  const map = this.make.tilemap({ key: "map"});
+  // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+  // Phaser's cache (i.e. the name you used in preload)
+  const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+
+  // Parameters: layer name (or index) from Tiled, tileset, x, y
+  const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+  const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+  const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
 }
 
 function update(time, delta) {
